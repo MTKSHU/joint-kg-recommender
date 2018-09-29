09d37330fb30efdc6ab0d462e22c25439d67db01
ethanMac <god13dog@163.com>
2018-09-30 01:02:46 +0800

    add visulizer curve options

--- jTransUP/utils/visuliazer.py
+++ jTransUP/utils/visuliazer.py
@@ -5,24 +5,29 @@ import numpy as np
 class Visualizer(object):
     def __init__(self, env='default', **kwargs):
         self.vis = visdom.Visdom(env=env, **kwargs)
-        self.index = {}         
-    def plot_many_stack(self, d):
+        self.index = {}
+
+    def plot_many_stack(self, points, win_name="", options={}):
         '''
         self.plot('loss',1.00)
         '''
-        name=list(d.keys())
-        name_total=" ".join(name)
-        x = self.index.get(name_total, 0)
-        val=list(d.values())
+        name=list(points.keys())
+        if len(win_name) < 1:
+            win_name = " ".join(name)
+
+        options['legend'] = name
+        options['title'] = win_name
+
+        x = self.index.get(win_name, 0)
+        val=list(points.values())
         if len(val)==1:
             y=np.array(val)
         else:
             y=np.array(val).reshape(-1,len(val))
-        #print(x)
+        
         self.vis.line(Y=y,X=np.ones(y.shape)*x,
-                    win=str(name_total),#unicode
-                    opts=dict(legend=name,
-                        title=name_total),
+                    win=win_name,
+                    opts=options,
                     update=None if x == 0 else 'append'
                     )
-        self.index[name_total] = x + 1            
+        self.index[win_name] = x + 1            
